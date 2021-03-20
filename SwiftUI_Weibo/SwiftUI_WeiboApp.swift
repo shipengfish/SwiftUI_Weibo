d3fd6db38e77b174cae9ea81cf828db24f8fc6a8
xiaokang <iishoni@163.com>
2021-03-20 22:41:37 +0800

    Initial Commit

---
+++ SwiftUI_Weibo/SwiftUI_WeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeiboApp.swift
+//  SwiftUI_Weibo
+//
+//  Created by admin on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
